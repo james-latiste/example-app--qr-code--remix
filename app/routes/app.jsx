9bb666928b24613fcd19372fda6157441eaf82c2
james <james.jang@latiste.com>
2023-10-19 13:42:51 -0700

    REST upload product with POST methods

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -4,8 +4,6 @@ import polarisStyles from "@shopify/polaris/build/esm/styles.css";
 import { boundary, shopifyApp } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { authenticate } from "../shopify.server";
-import { restResources } from "@shopify/shopify-api/rest/admin/2023-07";
-import { Shopify } from "@shopify/shopify-api";
 
 export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 export async function loader({ request }) {
@@ -13,35 +11,61 @@ export async function loader({ request }) {
   const { admin, session } = await authenticate.admin(request);
 
   // Use REST resources
-  const data = await admin.rest.resources.Product.count({ session });
-  // const client = await admin.rest.post({ path: "/product.json" });
-  // const product = await client.json();
+  // const data = await admin.rest.resources.Product.count({
+  //   session,
+  //   product_type: "dress",
+  // });
+
   const body = {
     product: {
       title: "Hiking backpack",
     },
   };
-  // product.title = "Burton Custom Freestyle 151";
-  // product.body_html = "<strong>Good snowboard!</strong>";
-  // product.vendor = "Burton";
-  // product.product_type = "Snowboard";
-  // product.status = "draft";
-  // await product.save({
-  //   update: true,
+
+  // const allProducts = await admin.rest.get({
+  //   path: "/products.json?",
   // });
+
+  // get all products
+  // const allProducts = await admin.rest.resources.Product.all({
+  //   session: session,
+  // });
+
+  // Session is built by the OAuth process
+
+  const product = new admin.rest.resources.Product({ session: session });
+  product.title = "Burton Custom Freestyle 151";
+  product.body_html = "<strong>Good snowboard!</strong>";
+  product.vendor = "Burton";
+  product.product_type = "Snowboard";
+  product.status = "draft";
+  await product.save({
+    update: true,
+  });
 
   return json({
     apiKey: process.env.SHOPIFY_API_KEY,
-    productCount: data.count,
+    // productCount: data.count,
+    // allProducts,
   });
 }
 
 export default function App() {
-  const { apiKey, productCount } = useLoaderData();
+  const {
+    apiKey,
+    // , productCount, allProducts
+  } = useLoaderData();
+  // const renderedProducts = allProducts.data.map((item, index) => (
+  //   <li key={index}>
+  //     {index} {JSON.stringify(item)}
+  //   </li>
+  // ));
 
   return (
     <AppProvider apiKey={apiKey} isEmbeddedApp>
-      Product.count <div>{productCount}</div>
+      {/* <div>Product.count: {productCount}</div>
+      <ul>{renderedProducts}</ul>
+      <div>product: {JSON.stringify(allProducts.data)}</div> */}
       <Outlet />
     </AppProvider>
   );
